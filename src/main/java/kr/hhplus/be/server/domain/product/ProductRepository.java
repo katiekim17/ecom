99a7e4ba7771ff0332b70b29dc8899912b47c909
YEOP <64470512+geonyeop123@users.noreply.github.com>
2025-04-11 03:59:26 +0900

    fix : repository 어노테이션 제거

--- src/main/java/kr/hhplus/be/server/domain/product/ProductRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/product/ProductRepository.java
@@ -1,11 +1,8 @@
 package kr.hhplus.be.server.domain.product;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface ProductRepository {
 
     public Optional<Product> find(Long productId);
