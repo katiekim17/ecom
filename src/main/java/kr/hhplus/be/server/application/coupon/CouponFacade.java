1cac14b4ea2ea292f743c21fe388d76116911079
YEOP <64470512+geonyeop123@users.noreply.github.com>
2025-05-09 06:57:22 +0900

    fix : 불필요한 로직 제거 및 스케줄러 시간 변경

--- src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
+++ src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
@@ -2,7 +2,6 @@ package kr.hhplus.be.server.application.coupon;
 
 import kr.hhplus.be.server.domain.coupon.Coupon;
 import kr.hhplus.be.server.domain.coupon.CouponService;
-import kr.hhplus.be.server.domain.user.UserService;
 import kr.hhplus.be.server.domain.userCoupon.UserCoupon;
 import kr.hhplus.be.server.domain.userCoupon.UserCouponService;
 import kr.hhplus.be.server.support.config.redis.DistributedLock;
@@ -14,9 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class CouponFacade {
-
-    //TODO 제거
-    private final UserService userService;
 
     private final CouponService couponService;
     private final UserCouponService userCouponService;
